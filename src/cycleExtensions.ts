3794732ab92d036e12c781ea17e1659ee97efbe7
Christopher Joseph Covert <covertops5@gmail.com>
2018-03-03 14:22:20 -0500

    Added a 'getSiblingFiles' method.

--- src/cycleExtensions.ts
+++ src/cycleExtensions.ts
@@ -2,6 +2,9 @@ import { fileNameCandidates, fileNameSplits } from "./pathMutations";
 
 import { Observable } from 'rxjs/Observable';
 import { concat, skip, skipWhile, takeWhile, shareReplay } from 'rxjs/operators';
+import { readdir, stat } from "fs";
+import { dirname } from "path";
+import { filter } from "./promiseUtil";
 
 export const getCycleCandidates = (
     path: string,
@@ -21,5 +24,32 @@ export const getCycleCandidates = (
 };
 
 export const cycleExtensions = async () => {
+    
+};
 
+const getSiblingFiles = async (path: string): Promise<string[]> => {
+    return new Promise<string[]>((resolve, reject) => {
+        readdir(dirname(path), async (err, files) => {
+            if (err) { 
+                reject(err);
+                return;
+            }
+
+            files = await filter(files, async (file) => !await isDirectory(file));
+            resolve(files);
+        });
+    });
+};
+
+const isDirectory = async (path: string): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+        stat(path, (err, stats) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve(stats.isDirectory());
+        });
+    });
 };
