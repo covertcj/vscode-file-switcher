58a341a855c0a7770f49141783ebdf1870f542b5
Christopher Joseph Covert <covertops5@gmail.com>
2018-03-04 16:44:37 -0500

    Removed old cycle code.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 'use strict';
 import * as vscode from 'vscode';
 import { regexSwitch } from './regexSwitch';
-import { cycleExtensions } from './cycleExtensions';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.file-switcher.regex-switch', regexSwitch));
-    context.subscriptions.push(vscode.commands.registerCommand('extension.file-switcher.cycle-extensions', cycleExtensions));
+    // context.subscriptions.push(vscode.commands.registerCommand('extension.file-switcher.cycle-extensions', cycleExtensions));
 }
 
 export function deactivate() {
