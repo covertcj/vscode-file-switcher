81e91572cb30852e30e14a241ff6f49100810db5
Christopher Joseph Covert <covertops5@gmail.com>
2018-03-04 19:54:19 -0500

    Implemented switchToExtension
    * No path mapping support.

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 'use strict';
 import * as vscode from 'vscode';
 import { regexSwitch } from './regexSwitch';
+import { switchToExtension } from './switchToExtension';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.file-switcher.regex-switch', regexSwitch));
     // context.subscriptions.push(vscode.commands.registerCommand('extension.file-switcher.cycle-extensions', cycleExtensions));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.file-switcher.switch-to-extension', switchToExtension));
 }
 
 export function deactivate() {
---
+++ src/switchToExtension.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+import { stripExtension, stripSuffixes, exists } from './pathUtil';
+
+export const switchToExtension = async (extension: string) => {
+    if (!extension) {
+        vscode.window.showErrorMessage('Expected an extension to switch to.');
+        return;
+    }
+
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showErrorMessage('You must have an active editor to switch files.');
+        return;
+    }
+    
+    const config = vscode.workspace.getConfiguration('fileSwitcher');
+    const closeCurrent = config.get('closeCurrent') as boolean;
+
+    const currentFile = editor.document.fileName;
+    const noExt = stripExtension(currentFile);
+    
+    const withNewExt = `${noExt}${extension}`;
+    if (await exists(withNewExt)) {
+        await openFile(withNewExt, closeCurrent);
+        return;
+    }
+
+    // TODO: Use the configuration to find the suffix list
+    const strippedSuffixes = stripSuffixes(noExt, config.get('strippableSuffixes') as string[]);
+    if (await exists(strippedSuffixes)) {
+        await openFile(strippedSuffixes, closeCurrent);
+        return;
+    }
+};
+
+const openFile = async (newFile: string, closeCurrent: boolean) => {
+    if (closeCurrent) {
+        vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+    }
+
+    const newDocument = await vscode.workspace.openTextDocument(newFile);
+    await vscode.window.showTextDocument(newDocument);
+};
